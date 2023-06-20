ee421ed6ccc1476e83f6a009b78e25ac4ac8bb00
Sai Shibu N B <saishibu38380@gmail.com>
2023-06-20 09:21:18 +0530

    Update CheckDevice.py

--- CheckDevice.py
+++ CheckDevice.py
@@ -1,6 +1,6 @@
 import tinytuya
 
-tinytuya.set_debug(True)
+# tinytuya.set_debug(True)
 
 c = tinytuya.Cloud(
         apiRegion="in", 
@@ -8,5 +8,8 @@ c = tinytuya.Cloud(
         apiSecret="cff6cca0f15346f682b59ea86123f56c", 
         apiDeviceID="8062300084cca891796a")
 
-devices = c.getdevices()
-print("Device List: %r" % devices)
+# devices = c.getdevices()
+# print("Device List: %r" % devices)
+
+result = c.getstatus(id)
+print("Status of device:\n", result)
