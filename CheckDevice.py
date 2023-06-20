754f4379c2f2a154ba9c85370b1f90076f3c817e
Sai Shibu N B <saishibu38380@gmail.com>
2023-06-20 09:19:07 +0530

    Update CheckDevice.py

--- CheckDevice.py
+++ CheckDevice.py
@@ -1,7 +1,8 @@
 import tinytuya
 
-d=tinytuya.OutletDevice("8062300084cca891796a", "192.168.1.113", "35f21f39b189da3d36d665a8d5113c5d")
-d.set_version(3.3)
+tinytuya.set_debug(True)
 
-data = d.status()
-print(data)
+c = tinytuya.Cloud()
+
+devices = c.getdevices()
+print("Device List: %r" % devices)
